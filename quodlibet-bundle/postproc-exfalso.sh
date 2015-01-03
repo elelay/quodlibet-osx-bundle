ddd8334c0d3378374bb9df41eea8dd51bd692003
Eric Le Lay <elelay@macports.org>
2015-01-03 15:58:57 +0100

    Quodlibet,ExFalso 3.3.0

--- quodlibet-bundle/postproc-exfalso.sh
+++ quodlibet-bundle/postproc-exfalso.sh
@@ -11,6 +11,7 @@ find $app/Contents/Resources/lib/python2.7 -name '*.pyo' -delete
 # check for dynamic linking consistency : nothing should reference gtk/inst
 find $app -name '*.so' -and -print -and  -exec sh -c 'otool -L $1 | grep /inst' '{}' '{}' ';'
 
+
 # Set the version and copyright automatically
 /usr/bin/xsltproc --stringparam app quodlibet -o $app/Contents/Info.plist info-plist.xsl Info-exfalso.plist
 
@@ -21,3 +22,6 @@ sed -i -e 's,bin/quodlibet,bin/exfalso,' $app/Contents/MacOS/ExFalso
 # exfalso icons
 rm $app/Contents/Resources/quodlibet.icns
 cp exfalso.icns $app/Contents/Resources/
+
+# list the provenance of every file in the bundle
+./provenance.pl $app > ~/Desktop/ExFalso.contents
